682177b153556473987c94aa9ed7206600f1bdde
Carolina Alves <carolinaalvesdesouza98@gmail.com>
2023-05-30 15:04:19 -0400

    Primeiro commit
    inicio da divisão de dois numeros

---
+++ Teste do Git/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Informe um valor: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Informe um valor: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int rt = a / b;
+Console.WriteLine($"O resultado da divisão é: {rt}");

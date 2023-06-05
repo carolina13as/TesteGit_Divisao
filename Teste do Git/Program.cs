7e758ad4bddf514de837dd700345a201ae7bdc06
Carolina Alves <carolinaalvesdesouza98@gmail.com>
2023-06-05 16:59:38 -0400

    Especificações do try e catch

--- Teste do Git/Program.cs
+++ Teste do Git/Program.cs
@@ -1,16 +1,25 @@
-﻿
+﻿using System;
+
+
 try 
 {
     Console.Write("Informe um valor: ");
     int a = Convert.ToInt32(Console.ReadLine());
     Console.Write("Informe um valor: ");
     int b = Convert.ToInt32(Console.ReadLine());
-
     int rt = a / b;
     Console.WriteLine($"O resultado da divisão é: {rt}");
 }
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine($"\nDvisão por zero é invalido!\n{ex.Message} - Menssagem de erro");
+}
+catch(InvalidCastException ex)
+{
+    Console.WriteLine($"\nDvisão por letra é invalida, não se pode converte letra em número!\n{ex.Message} - Menssagem de erro");
+}
 catch(Exception ex)
 {
-    Console.WriteLine("\nDvisão Invalida!");
+    Console.WriteLine($"\nOcorreu algum erro!\n{ex.Message} - Menssagem de erro");
 }
 
